a28c7fa04ca73988976b43c3171f75b473945e1f
Rajudevo <rajucse093@gmail.com>
2021-09-29 18:16:23 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/Card';
 
 function App() {
+  const items = [
+    {name:'first-item', description:'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt-JmDfLz7ErRiTZ9vIme55A9JGQqdx8qJ_xQ_lB2UIqGAFELpsKQQ8xuTSrlqrly-tSQ&usqp=CAU'},
+    {name:'first-item', description:'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt-JmDfLz7ErRiTZ9vIme55A9JGQqdx8qJ_xQ_lB2UIqGAFELpsKQQ8xuTSrlqrly-tSQ&usqp=CAU'},
+    {name:'first-item', description:'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt-JmDfLz7ErRiTZ9vIme55A9JGQqdx8qJ_xQ_lB2UIqGAFELpsKQQ8xuTSrlqrly-tSQ&usqp=CAU'},
+    {name:'first-item', description:'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt-JmDfLz7ErRiTZ9vIme55A9JGQqdx8qJ_xQ_lB2UIqGAFELpsKQQ8xuTSrlqrly-tSQ&usqp=CAU'},
+    {name:'first-item', description:'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt-JmDfLz7ErRiTZ9vIme55A9JGQqdx8qJ_xQ_lB2UIqGAFELpsKQQ8xuTSrlqrly-tSQ&usqp=CAU'},
+    {name:'first-item', description:'This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.', img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt-JmDfLz7ErRiTZ9vIme55A9JGQqdx8qJ_xQ_lB2UIqGAFELpsKQQ8xuTSrlqrly-tSQ&usqp=CAU'}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="row row-cols-1 row-cols-md-6 g-4">
+    {
+      items.map(item => <Card
+      item ={item}
+      ></Card>)
+    }
+    
+  </div>
   );
 }
 
---
+++ src/components/Card/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Card = (props) => {
+    console.log(props)
+
+    const{name, img, description} = props.item;
+    return (
+        
+            <div className="col">
+      <div className="card">
+        <img src={img} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{description}</p>
+        </div>
+      </div>
+    </div>
+        
+    );
+};
+
+export default Card;
